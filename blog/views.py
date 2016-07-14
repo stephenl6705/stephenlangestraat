8dee51f52b5abb4a52c661708f85fa407566a41a
stephenl6705 <stephen.langestraat@gmail.com>
2016-07-14 12:41:39 +0000

    Sharing posts by e-mail using a form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -33,3 +35,22 @@ def post_detail(request, year, month, day, post):
 
 def home(request):
     return redirect('blog/')
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    who = 'Nobody'
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            who = cd['to']
+            send_mail(subject, message, cd['name'], [who])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent, 'who': who})
--- mysite/settings.py
+++ mysite/settings.py
@@ -102,3 +102,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'stephen.langestraat@gmail.com'
+EMAIL_HOST_PASSWORD = 'gp3862gp'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
