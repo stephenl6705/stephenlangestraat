5e510031a03db4b7884c1b9507857266feacebd7
stephenl6705 <stephen.langestraat@gmail.com>
2016-07-18 11:15:37 +0000

    include tags in models.py and list.html

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -26,6 +27,7 @@ class Post(models.Model):
         default='draft')
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
